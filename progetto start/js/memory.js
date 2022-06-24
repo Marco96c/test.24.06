30f9263dd14b47661c48a120c41c0b87451d1292
Marco Castaldo <102226725+Marco96c@users.noreply.github.com>
2022-06-24 16:50:53 +0200

    Add files via upload

---
+++ progetto start/js/memory.js
@@ -0,0 +1,143 @@
+let arrayAnimali = ['🐱', '🦉', '🐾', '🦁', '🦋', '🐛', '🐝', '🐬', '🦊', '🐨', '🐰', '🐯', '🐱', '🦉', '🐾', '🦁', '🦋', '🐛', '🐝', '🐬', '🦊', '🐨', '🐯', '🐰'];
+//libreria per icone
+//https://html-css-js.com/html/character-codes/
+
+
+let arrayComparison = [];   // non so ma non va
+
+document.body.onload = startGame();
+
+var interval; // 1. interval
+var iconsFind = document.getElementsByClassName("find");  // 2. una agganciata alla classe find
+var modal = document.getElementById('modal'); //3. una agganciata al'id modal
+var timer = document.querySelector(".timer"); //4. una agganciata alla classe timer
+
+
+//una funzione che serve a mescolare in modo random gli elementi dell'array che viene passato 
+// (l'array contiene le icone degli animali)
+function shuffle(a) {
+    var currentIndex = a.length;
+    var temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = a[currentIndex];
+        a[currentIndex] = a[randomIndex];
+        a[randomIndex] = temporaryValue;
+    }
+    return a;
+}
+  function playAgain(){       // una funzione che rimuove la classe active e chiama la funzione startGame()
+    modal.classList.remove("active");
+    startGame();
+  } 
+
+  function startGame(){
+    clearInterval(interval);
+    arrayConfronto = [];
+    var arrayShuffle = shuffle(arrayAnimali);
+  
+    var lista = document.getElementById('griglia');
+    while (lista.hasChildNodes()) {  
+      lista.removeChild(lista.firstChild);
+    }
+  
+    for(var i = 0; i < 24; i++){    
+      // var id = 'icon-' + i;
+      var box = document.createElement('div');
+      var element = document.createElement('div');
+      element.className = 'icon';
+      document.getElementById('griglia').appendChild(box).appendChild(element);
+      element.innerHTML = arrayShuffle[i];
+    }
+  }
+// la funzione startGame che pulisce il timer, dichiara un array vuoto, mescola casualmente l'array degli animali
+// (var arrayShuffle = shuffle(arrayAnimali);), aggancia il contenitore con id griglia, 
+// pulisce tutti gli elementi che eventualmente contiene
+// poi fa ciclo per creare i 24 div child -> aggiunge la class e l'elemento dell'array in base all'indice progressivo
+// chiama la funzione timer e associa a tutti gli elementi (div) di classe icon l'evento click e le due funzioni definit sotto
+
+///////////////////////////////////////////////////////
+
+/*
+        var icon = document.getElementsByClassName("icon");
+        var icons = [...icon];
+        è uguale a 
+        var icons = document.getElementsByClassName("icon");
+        //var icons = [...icon];
+        è un operatore che serve per passare un array come argomento:
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax 
+        https://www.tutorialspoint.com/es6/es6_operators.htm (cerca spread nella pagina)
+        */
+
+function displayIcon() {
+    var icon = document.getElementsByClassName("icon");
+    var icons = [...icon];
+
+    //mette/toglie la classe show
+    this.classList.toggle("show");
+    //aggiunge l'oggetto su cui ha cliccato all'array del confronto
+    arrayComparison.push(this);
+
+    var len = arrayComparison.length;
+    //se nel confronto ci sono due elementi
+    if (len === 2) {
+        //se sono uguali aggiunge la classe find
+        if (arrayComparison[0].innerHTML === arrayComparison[1].innerHTML) {     
+            arrayComparison[0].classList.add("find", "disabled");
+            arrayComparison[1].classList.add("find", "disabled");
+            arrayComparison = [];
+        } else {
+            //altrimenti (ha sbagliato) aggiunge solo la classe disabled
+            icons.forEach(function(item) {
+                item.classList.add('disabled');
+            });
+            // con il timeout rimuove  la classe show per nasconderli
+            setTimeout(function() {
+                arrayComparison[0].classList.remove("show");
+                arrayComparison[1].classList.remove("show");
+                icons.forEach(function(item) {
+                    item.classList.remove('disabled');
+                    for (var i = 0; i < iconsFind.length; i++) {
+                        iconsFind[i].classList.add("disabled");
+                    }
+                });
+                arrayComparison = [];
+            }, 700);
+        }
+    }
+}
+function openModal(){  
+    if (iconsFind.length == 24){
+      clearInterval(interval);
+      modal.classList.add("active");
+      document.getElementById("tempoTrascorso").innerHTML = timer.innerHTML;
+      closeModal();
+    }
+  }
+  function closeModal(){  
+    closeicon.addEventListener("click", function(e){
+      modal.classList.remove("active");
+      startGame();
+    });
+  }
+
+  function startTimer(){
+    var s = 0; var  m = 0; var h = 0;
+    interval = setInterval(function(){
+    timer.innerHTML = 'Tempo: ' + m + " min " + s + " sec";
+      s++;
+      if(s == 60){
+        m++;
+        s = 0;
+      }
+      if(m == 60){
+        h++;
+        m = 0;
+      }
+    },1000);
+  }
+//una funzione che viene mostrata alla fine quando sono tutte le risposte esatte
+
+// una funzione che calcola il tempo e aggiorna il contenitore sotto
